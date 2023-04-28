0e3c31dd4e91ea13345dfb2bc918abcf54089b91
Bpolus <bpoluhovich2312@gmail.com>
2023-04-28 20:06:31 +0300

    v11.30213165

--- dock/v2/cal.py
+++ dock/v2/cal.py
@@ -1,25 +1,20 @@
 import calendar
 
 while True:
-    # Ask the user which month to display the calendar for
     choice = input("Hello there!!! \nEnter '1' to display the calendar for the current month, or '2' to display the calendar for another month, or 'q' to quit: ")
 
-    # If the user chooses to quit
     if choice == 'q':
         break
 
-    # If the user chooses the current month
     elif choice == '1':
         year = calendar.datetime.datetime.now().year
         month = calendar.datetime.datetime.now().month
         print(calendar.month(year, month))
 
-    # If the user chooses another month
     elif choice == '2':
         year = int(input("Enter the year: "))
         month = int(input("Enter the month (1-12): "))
         print(calendar.month(year, month))
 
-    # If the user enters an invalid choice
     else:
         print("Invalid choice.")
